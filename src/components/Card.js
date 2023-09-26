bf5168d1b2e466acea328c8d4be3696012ce3b7e
nfluk <nickylukic@gmail.com>
2023-09-26 16:46:02 +0200

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Card() {
+  return (
+    <div>
+      <img src="#" alt="#" />
+      <div>
+        <h2>place holder name</h2>
+        <h3>place holder position</h3>
+        <p>email</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './navbar.css';
+import 'animate.css';
+
+function Navbar() {
+  const teamType = 'sales';
+
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <h3 className={'animate__animated animate__backInLeft'}>
+        {teamType.toUpperCase()}
+      </h3>
+    </div>
+  );
+}
+
+export default Navbar;
