421d9dd1c009ea007a5e311f7553ec55ace849e7
nfluk <nickylukic@gmail.com>
2023-09-27 12:57:44 +0200

    minor cleanup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,12 @@ import './navbar.css';
 import 'animate.css';
 import SearchBox from './SearchBox';
 
-function Navbar({ onSearchChange }) {
-  const teamType = 'Division: sales';
-
+function Navbar({ onSearchChange, teamType }) {
   return (
     <div className="ma2">
       <h1 className="f1 dark-blue">Our Team</h1>
       <h2 className="animate__animated animate__backInLeft f5 ttu blue ">
-        {teamType}
+        Division: Sales
       </h2>
       <SearchBox onSearchChange={onSearchChange} />
     </div>
